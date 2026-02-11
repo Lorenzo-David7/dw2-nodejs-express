9d74f59a8bf773c92532a2086c2893dbc4026fd6
DSM2 <fatec-dsm2@academico.intra>
2026-02-11 20:53:15 -0300

    Aula 01 - Variáveis

---
+++ js-aula-01-tipos-de-funcoes/variaveis/script.js
@@ -0,0 +1,16 @@
+// Comentário de linha
+
+var cidade = "Registro";
+var cidade = "Sete Barras";
+
+document.write(cidade);
+
+let nome = "Lorenzo";
+nome = "Maria";
+document.write("<br>");
+document.write(nome);
+
+const pais = "Brasil";
+pais = "Argentina";
+document.write("<br>");
+document.write(nome);
